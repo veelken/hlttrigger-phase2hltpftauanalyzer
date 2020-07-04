327fc6467a27068716c15166095e4f0f7d6e61ec
Christian Veelken <christian.veelken@cern.ch>
2020-07-04 13:12:46 +0300

    added template instantiation for jets

--- plugins/ParticleAntiOverlapSelector.cc
+++ plugins/ParticleAntiOverlapSelector.cc
@@ -8,6 +8,8 @@
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h" // reco::PFCandidate
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"        // reco::GenParticle
 #include "DataFormats/JetReco/interface/GenJet.h"                    // reco::GenJet
+#include "DataFormats/PatCandidates/interface/Jet.h"                 // pat::Jet
+#include "DataFormats/JetReco/interface/PFJet.h"                     // reco::PFJet
 
 typedef ObjectSelector<ParticleAntiOverlapSelector<pat::Tau>> PATTauAntiOverlapSelector;
 typedef ObjectSelector<ParticleAntiOverlapSelector<pat::PackedCandidate>> PackedCandidateAntiOverlapSelector;
@@ -15,6 +17,8 @@ typedef ObjectSelector<ParticleAntiOverlapSelector<reco::PFTau>> PFTauAntiOverla
 typedef ObjectSelector<ParticleAntiOverlapSelector<reco::PFCandidate>> PFCandidateAntiOverlapSelector;
 typedef ObjectSelector<ParticleAntiOverlapSelector<reco::GenParticle>> GenParticleAntiOverlapSelector;
 typedef ObjectSelector<ParticleAntiOverlapSelector<reco::GenJet>> GenJetAntiOverlapSelector;
+typedef ObjectSelector<ParticleAntiOverlapSelector<pat::Jet>> PATJetAntiOverlapSelector;
+typedef ObjectSelector<ParticleAntiOverlapSelector<reco::PFJet>> PFJetAntiOverlapSelector;
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
@@ -24,3 +28,5 @@ DEFINE_FWK_MODULE(PFTauAntiOverlapSelector);
 DEFINE_FWK_MODULE(PFCandidateAntiOverlapSelector);
 DEFINE_FWK_MODULE(GenParticleAntiOverlapSelector);
 DEFINE_FWK_MODULE(GenJetAntiOverlapSelector);
+DEFINE_FWK_MODULE(PATJetAntiOverlapSelector);
+DEFINE_FWK_MODULE(PFJetAntiOverlapSelector);
